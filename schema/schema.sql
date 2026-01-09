c79ff84b3cb421cb2089a404c4746f304578d0ad
Tom Egan <tom@tomegan.tech>
2026-01-09 12:08:19 -0500

    feature: rough badge storage

--- schema/schema.sql
+++ schema/schema.sql
@@ -498,4 +498,19 @@ CREATE TABLE schema_versioning (
 	PRIMARY KEY(id)
 );
 
-INSERT INTO schema_versioning(version, comment) VALUES ("2.10.0", "Database created");
+CREATE TABLE badges (
+	id INT UNSIGNED AUTO_INCREMENT NOT NULL,
+	name TEXT NOT NULL,
+	PRIMARY KEY(id)
+);
+
+CREATE TABLE badges_x_equipment_types (
+	id INT UNSIGNED AUTO_INCREMENT NOT NULL,
+	badge_id INT UNSIGNED NOT NULL,
+	equipment_type_id INT UNSIGNED NOT NULL,
+	PRIMARY KEY(id),
+	FOREIGN KEY badges_x_equipment_types_equipment_type_id (equipment_type_id) REFERENCES equipment_types (id),
+	FOREIGN KEY badges_x_equipment_types_badge_id (badge_id) REFERENCES badges (id) ON DELETE CASCADE
+);
+
+INSERT INTO schema_versioning(version, comment) VALUES ("2.11.0", "Database created");
